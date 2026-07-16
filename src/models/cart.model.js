ec3fab1b612a777607bb51a23c4fb33c2474a65c
mohaimen <mohaimen707@gmail.com>
2026-07-17 02:39:32 +0600

    cart model schema added

---
+++ src/models/cart.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+const cartSchema = new Schema(
+    {
+        user: {
+            type: Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        product: {
+            type: Types.ObjectId,
+            ref: "Product",
+            required: true,
+        },
+
+        variantId: {
+            type: Types.ObjectId,
+            required: true,
+        },
+
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1,
+            default: 1,
+        },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+cartSchema.index(
+    {
+        user: 1,
+        variantId: 1,
+    },
+    {
+        unique: true,
+    }
+);
+
+cartSchema.index({
+    user: 1,
+});
+
+module.exports = mongoose.model("Cart", cartSchema)
