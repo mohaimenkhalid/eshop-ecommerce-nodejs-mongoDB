6554761dbd2d1fe2de0982397aa86f5a7773a2b8
mohaimen <mohaimen707@gmail.com>
2026-07-19 00:22:35 +0600

    update user migration for refreshToken api

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,6 +54,10 @@ const userSchema = new mongoose.Schema(
             type: Boolean,
             default: false,
         },
+        refreshToken: {
+            type: String,
+            default: null,
+        }
     },
     {
         timestamps: true,
